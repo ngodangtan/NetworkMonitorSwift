42a49d38cfef7878eb0883d97dc76dbfccd80b5d
27293 <myltd6@fpt.com.vn>
2020-12-07 21:46:35 +0700

    Initial Commit

---
+++ NetworkMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkMonitor
+//
+//  Created by Ngo Dang tan on 07/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
